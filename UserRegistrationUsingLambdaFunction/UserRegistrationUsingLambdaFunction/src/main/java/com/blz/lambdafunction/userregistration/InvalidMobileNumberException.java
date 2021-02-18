7a4dfd409d09033735fe52ac9357c3ef68276cbc
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-18 12:16:03 +0530

    Delete InvalidMobileNumberException.java

--- UserRegistrationUsingLambdaFunction/UserRegistrationUsingLambdaFunction/src/main/java/com/blz/lambdafunction/userregistration/InvalidMobileNumberException.java
+++
@@ -1,9 +0,0 @@
-package com.blz.lambdafunction.userregistration;
-
-@SuppressWarnings("serial")
-public class InvalidMobileNumberException extends Exception
-{
-	public InvalidMobileNumberException(String message) {
-		super(message);
-	}
-}
